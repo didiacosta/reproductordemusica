edc98fede31cb4e8ba9fdada86dc7a90b615edaa
didiacosta <didi_acosta@hotmail.com>
2020-11-20 12:07:53 -0500

    implementacion del str

--- artista/models.py
+++ artista/models.py
@@ -5,5 +5,8 @@ class Artista(models.Model):
 	nombre = models.CharField(max_length=100)
 	nombreArtistico = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.nombre
+
 	class Meta:
 		db_table = 'artista_artista'
